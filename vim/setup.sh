31aafe26b12fb41c3c85facdb146d395c85f7dd7
wangcong18 <wangcong18@xiaomi.com>
2020-01-20 19:27:37 +0800

    add clang-format

--- vim/setup.sh
+++ vim/setup.sh
@@ -3,7 +3,10 @@
 TASK="setup SpaceVim"
 CURRENT="$( cd "$(dirname "$0")" && pwd -P )"
 
+echo "start install SpaceVim"
+curl -sLf https://spacevim.org/install.sh | bash
+
 echo "$TASK" start
-rm -f "$HOME"/.SpaceVim.d && ln -s "$CURRENT"/.SpaceVim.d "$HOME"/.SpaceVim.d
+rm -rf "$HOME"/.SpaceVim.d && ln -s "$CURRENT"/.SpaceVim.d "$HOME"/.SpaceVim.d
 echo "$TASK" finish
 
