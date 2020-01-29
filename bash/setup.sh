53d1c2c312d920f30e83f76957f8c769cec83715
WangCong <1018957763@qq.com>
2020-01-29 17:47:32 +0800

    add plugin for zsh

--- bash/setup.sh
+++ bash/setup.sh
@@ -5,4 +5,7 @@ CURRENT="$( cd "$(dirname "$0")" && pwd -P )"
 
 echo "$TASK" start
 rm -f "$HOME"/.zshrc && ln -s "$CURRENT"/.zshrc "$HOME"/.zshrc
+brew install autojump
+brew install zsh-syntax-highlighting
+brew install zsh-autosuggestions
 echo "$TASK" finish
