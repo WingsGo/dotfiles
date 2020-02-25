2982a3e5e8349f5c8759a55c7d308552de8bfb30
wangcong <wangcong18@xiaomi.com>
2020-02-25 16:36:36 +0800

    Fix bashrc for diff os

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,11 +1,15 @@
 #! /usr/bin/env bash
 
-TASK="setup zsh"
+TASK="setup sh"
 CURRENT="$( cd "$(dirname "$0")" && pwd -P )"
 
 echo "$TASK" start
-rm -f "$HOME"/.zshrc && ln -s "$CURRENT"/.zshrc "$HOME"/.zshrc
-brew install autojump
-brew install zsh-syntax-highlighting
-brew install zsh-autosuggestions
+rm -f "$HOME"/.bashrc && ln -s "$CURRENT"/.bashrc "$HOME"/.bashrc
+if [[ $(uname -m) == "Darwin" ]];then
+  brew install autojump
+  brew install zsh-syntax-highlighting
+  brew install zsh-autosuggestions
+fi
+source "$HOME"/.bashrc
+
 echo "$TASK" finish
